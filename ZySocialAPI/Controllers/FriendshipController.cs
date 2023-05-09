ffbe12187cef156fac787967fd3d8cd023177158
oZygro <zygrobusiness@gmail.com>
2023-05-08 21:14:25 -0700

    Added controllers for friendships and users

---
+++ ZySocialAPI/Controllers/FriendshipController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZySocialAPI.Data;
+using ZySocialAPI.Models;
+using ZySocialAPI.Models.Custom;
+
+namespace ZySocialAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FriendshipController : Controller
+    {
+        private ZySocialDbContext _context;
+        public FriendshipController(ZySocialDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<IEnumerable<SimpleFriendRequest>>> GetSimpleFriendRequests()
+        {
+            try
+            {
+                var friendRequests = await _context.FriendRequests
+                    .Select(fr => new SimpleFriendRequest
+                    {
+                        UserSenderId = fr.UserSenderId,
+                        UserReceiverId = fr.UserReceiverId,
+                        Accepted = fr.Accepted,
+                        Responded = fr.Responded,
+                        FriendRequestId = fr.FriendRequestId,
+                        SendDate = fr.SendDate
+                    })
+                    .ToListAsync();
+
+                if (friendRequests == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(friendRequests);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
+                return StatusCode(500, "A server-side error occurred.");
+            }
+        }
+
+
+    }
+}
--- ZySocialAPI/Controllers/UserController.cs
+++ ZySocialAPI/Controllers/UserController.cs
@@ -18,22 +18,30 @@ namespace ZySocialAPI.Controllers
         [HttpGet("[action]")]
         public async Task<ActionResult<IEnumerable<SimpleUser>>> GetSimpleUsers()
         {
-            var users = _context.Users.Select(u => new SimpleUser
+            try
             {
-                UserId = u.UserId,
-                Name = u.Name,
-                Password = u.Password,
-                Email = u.Email,
-                PhoneNumber = u.PhoneNumber,
-                ProfilePicture = u.ProfilePicture
-            }).ToList();
+                var users = _context.Users.Select(u => new SimpleUser
+                {
+                    UserId = u.UserId,
+                    Name = u.Name,
+                    Password = u.Password,
+                    Email = u.Email,
+                    PhoneNumber = u.PhoneNumber,
+                    ProfilePicture = u.ProfilePicture
+                }).ToList();
 
-            if (users == null)
-            {
-                return NotFound();
+                if (users == null)
+                {
+                    return NotFound();
+                }
+                return Ok(users);
             }
-
-            return Ok(users);
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
+                return StatusCode(500, "A server-side error occurred.");
+            }
+            
         }
 
     }
