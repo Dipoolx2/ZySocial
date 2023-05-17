a0eb8469a0701e0354f968d1e350b99c94c581ad
Dipoolx2 <89845880+Dipoolx2@users.noreply.github.com>
2023-05-16 17:31:09 -0700

    Added basic view content

--- FinalProject/FinalProject/Views/ProfileEditView.swift
+++ FinalProject/FinalProject/Views/ProfileEditView.swift
@@ -12,7 +12,55 @@ struct ProfileEditView: View {
     var userId: Int64
     
     var body: some View {
-        // Your ProfileEditView implementation
-        Text("Profile Edit View for " + String(userId))
+        VStack(spacing: 30) {
+            Spacer()
+            
+            Button(action: {
+                // Handle Edit Email action
+            }) {
+                Text("Edit Email")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+            }
+            
+            Button(action: {
+                // Handle Edit Phone Number action
+            }) {
+                Text("Edit Phone Number")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+            }
+            
+            Button(action: {
+                // Handle Edit Profile Picture action
+            }) {
+                Text("Edit Profile Picture")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding(.horizontal)
+            }
+            
+            Spacer()
+        }
+        .padding(.vertical)
+        .navigationBarTitle("Edit Profile")
+        .navigationBarItems(trailing: Button(action: {
+            // Handle Submit action
+        }) {
+            Text("Submit")
+                .foregroundColor(.blue)
+                .padding()
+        })
     }
 }
