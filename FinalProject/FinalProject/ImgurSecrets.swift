19e71fff1f496d610e42aca8c1fade25d24e3d4d
Dipoolx2 <89845880+Dipoolx2@users.noreply.github.com>
2023-05-16 19:58:23 -0700

    added imgursecrets

---
+++ FinalProject/FinalProject/ImgurSecrets.swift
@@ -0,0 +1,9 @@
+//
+//  ImgurSecrets.swift
+//  FinalProject
+//
+//  Created by Student on 5/16/23.
+//
+
+import Foundation
+
--- FinalProject/FinalProject/Services/ImageService.swift
+++ FinalProject/FinalProject/Services/ImageService.swift
@@ -6,8 +6,42 @@
 //
 
 import Foundation
-import SwiftUI
+import UIKit
 
-func uploadImage(image: Image) async -> String {
+public func uploadImage(image: UIImage) async -> String? {
+    do {
+        if let imageURL = try await uploadImageToImgur(image: image) {
+            print("Image uploaded successfully. URL: \(imageURL)")
+            return imageURL
+        } else {
+            print("Failed to upload image.")
+            return nil
+        }
+    } catch {
+        print("Error: \(error)")
+        return nil
+    }
+}
+
+private func uploadImageToImgur(image: UIImage) async throws -> String? {
+    guard let imageData = image.jpegData(compressionQuality: 0.8) else {
+        return nil
+    }
     
+    let url = URL(string: "https://api.imgur.com/3/image")!
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    
+    request.setValue("Client-ID \()", forHTTPHeaderField: "Authorization") // Replace YOUR_CLIENT_ID with the actual client ID obtained from Imgur.
+    request.httpBody = imageData
+    
+    let (data, _) = try await URLSession.shared.data(for: request)
+    
+    if let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+       let imageData = json["data"] as? [String: Any],
+       let link = imageData["link"] as? String {
+        return link
+    } else {
+        return nil
+    }
 }
