e200a586967991cc9cfa6bc2c2aee3975e7ec875
Dipoolx2 <89845880+Dipoolx2@users.noreply.github.com>
2023-05-16 17:46:53 -0700

    Changed basic fields

--- FinalProject/FinalProject/Views/ProfileEditView.swift
+++ FinalProject/FinalProject/Views/ProfileEditView.swift
@@ -10,57 +10,84 @@ import SwiftUI
 
 struct ProfileEditView: View {
     var userId: Int64
+    @State private var email: String = ""
+    @State private var phoneNumber: String = ""
+    @State private var selectedProfilePicture: String = ""
     
     var body: some View {
-        VStack(spacing: 30) {
-            Spacer()
-            
-            Button(action: {
-                // Handle Edit Email action
-            }) {
-                Text("Edit Email")
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
+        VStack {
+            VStack {
+                HStack {
+                    TextField("Change Email", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    Button(action: {
+                        // Perform actions when the "Change Email" button is tapped
+                        // You can add your logic here to handle the email change
+                        updateEmail()
+                    }) {
+                        Text("Change")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .padding(.trailing)
+                }
+                
+                HStack {
+                    TextField("Change Phone Number", text: $phoneNumber)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    
+                    Button(action: {
+                        // Perform actions when the "Change Phone Number" button is tapped
+                        // You can add your logic here to handle the phone number change
+                        updatePhoneNumber()
+                    }) {
+                        Text("Change")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .padding(.trailing)
+                }
+                
+                HStack {
+                    Text("Select new profile picture")
+                        .padding()
+                    
+                    Button(action: {
+                        // Perform actions when the "Change Profile Picture" button is tapped
+                        // You can add your logic here to handle the profile picture change
+                        selectProfilePicture()
+                    }) {
+                        Text("Change")
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                    .padding(.trailing)
+                }
             }
-            
-            Button(action: {
-                // Handle Edit Phone Number action
-            }) {
-                Text("Edit Phone Number")
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-            }
-            
-            Button(action: {
-                // Handle Edit Profile Picture action
-            }) {
-                Text("Edit Profile Picture")
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-                    .padding(.horizontal)
-            }
-            
-            Spacer()
         }
-        .padding(.vertical)
-        .navigationBarTitle("Edit Profile")
-        .navigationBarItems(trailing: Button(action: {
-            // Handle Submit action
-        }) {
-            Text("Submit")
-                .foregroundColor(.blue)
-                .padding()
-        })
+    }
+    
+    private func updateEmail() {
+        // Implement the logic to update the user's email
+        // using the `email` property
+    }
+    
+    private func updatePhoneNumber() {
+        // Implement the logic to update the user's phone number
+        // using the `phoneNumber` property
+    }
+    
+    private func selectProfilePicture() {
+        // Implement the logic to select a new profile picture
+        // You can add your own implementation or use a library/component for picking images
     }
 }
